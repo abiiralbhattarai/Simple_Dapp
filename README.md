5766b8cf02b1a3bac5b7da4ce97e25795cdc9de0
abiiralbhattarai <bhattarai.abi07@gmail.com>
2022-05-24 09:13:38 +0545

    Work in Progress

---
+++ README.md
@@ -0,0 +1,16 @@
+# Simple Daap project using React Typescript, Harhat and ether js.
+
+I have learned to interact with smart contract using ether js
+
+
+Try running some of the following tasks:
+
+```shell
+npx hardhat accounts
+npx hardhat compile
+npx hardhat clean
+npx hardhat test
+npx hardhat node
+node scripts/sample-script.js
+npx hardhat help
+```
---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import {useState,useEffect} from 'react';
+import Greeter from "./artifacts/contracts/Greeter.sol/Greeter.json";
+import {ethers} from "ethers";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
